d899182ccf0b73d0868a3a1620a497a33bbb98c6
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-09-06 10:22:35 +0700

    Learn Hook useSpring part 1

--- src/ReactSpring/DemoUseSpring.js
+++ src/ReactSpring/DemoUseSpring.js
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { useSpring, animated } from "react-spring";
 export default function DemoUseSpring(props) {
+  const propsAnimation = useSpring({
+    color: "red",
+    from: { color: "green" },
+    config: { duration: 1000 },
+  });
   return (
-    <div>DemoUseSpring</div>
-  )
+    <animated.div style={propsAnimation}>
+      <h1 className="display-4"> Demo useSpring</h1>
+    </animated.div>
+  );
 }
