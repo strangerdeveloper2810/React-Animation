e8a24c10f988ad6f8b3012d6533b88e0eb7ed0a7
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-09-07 16:39:02 +0700

    Learn Hook useSprings

--- src/ReactSpring/DemoReactSpring.js
+++ src/ReactSpring/DemoReactSpring.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import DemoUseSprings from './DemoUseSprings'
 // import DemoUseSpring from './DemoUseSpring'
-import Ex2UseSpring from './Ex2UseSpring'
+// import Ex2UseSpring from './Ex2UseSpring'
 
 export default function DemoReactSpring(props) {
   return (
     <div>
         {/* <DemoUseSpring/> */}
-        <Ex2UseSpring/>
+        {/* <Ex2UseSpring/> */}
+        <DemoUseSprings/>
     </div>
   )
 }
